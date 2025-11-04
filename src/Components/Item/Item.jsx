f83637423af4470acf2fc9413af1d7220b88623b
Nathanael Araújo Da Silva <nathanael.araujodasilva@kantar.com>
2025-11-04 16:36:18 +0000

    update compoments

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,17 +6,11 @@ import { backend_url, currency } from '../../App'
 const Item = (props) => {
   return (
     <div data-qa-locator={"product-item"} className='item'>
-      <Link to={`/product/${props.id}`}>
-        <img onClick={window.scrollTo(0, 0)} src={backend_url+props.image} alt="products" />
-      </Link>
-      <div className="item-content">
-        <p>{props.name}</p>
-        <div className="item-prices">
-          <div className="item-price-new">{currency}{props.new_price}</div>
-          {props.old_price && props.old_price !== props.new_price && (
-            <div className="item-price-old">{currency}{props.old_price}</div>
-          )}
-        </div>
+      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0, 0)} src={backend_url+props.image} alt="products" /></Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{currency}{props.new_price}</div>
+        <div className="item-price-old">{currency}{props.old_price}</div>
       </div>
     </div>
   )
