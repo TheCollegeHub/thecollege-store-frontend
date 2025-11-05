a4420ae1945b51ab180b98afb7dcec3f8143154b
Nathanael Araújo Da Silva <nathanael.araujodasilva@kantar.com>
2025-11-05 12:58:30 +0000

    add to cart button implementation

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -22,44 +22,45 @@ const Item = (props) => {
     
     if (result && result.status === 200) {
       setShowToast(true)
-      setTimeout(() => setShowToast(false), 2000)
+      setTimeout(() => setShowToast(false), 3000)
     }
     
     setIsAdding(false)
   }
 
   return (
-    <div data-qa-locator={"product-item"} className='item'>
-      <Link to={`/product/${props.id}`} className="item-link">
-        <div className="item-image-container">
-          <img onClick={window.scrollTo(0, 0)} src={backend_url+props.image} alt="products" />
-          <button 
-            className={`add-to-cart-btn ${isAdding ? 'adding' : ''} ${showToast ? 'added' : ''}`}
-            onClick={handleAddToCart}
-            disabled={isAdding}
-          >
-            {showToast ? (
-              <>
-                <CheckCircleIcon sx={{ fontSize: 18 }} />
-                <span>Added!</span>
-              </>
-            ) : (
-              <>
-                <ShoppingCartIcon sx={{ fontSize: 18 }} />
-                <span>Add to Cart</span>
-              </>
-            )}
-          </button>
-        </div>
-        <div className="item-info">
-          <p>{props.name}</p>
-          <div className="item-prices">
-            <div className="item-price-new">{currency}{props.new_price}</div>
-            <div className="item-price-old">{currency}{props.old_price}</div>
+    <>
+      <div data-qa-locator={"product-item"} className='item'>
+        <Link to={`/product/${props.id}`} className="item-link">
+          <div className="item-image-container">
+            <img onClick={window.scrollTo(0, 0)} src={backend_url+props.image} alt="products" />
+            <button 
+              className={`add-to-cart-btn ${isAdding ? 'adding' : ''}`}
+              onClick={handleAddToCart}
+              disabled={isAdding}
+            >
+              <ShoppingCartIcon sx={{ fontSize: 18 }} />
+              <span>{isAdding ? 'Adding...' : 'Add to Cart'}</span>
+            </button>
+          </div>
+          <div className="item-info">
+            <p>{props.name}</p>
+            <div className="item-prices">
+              <div className="item-price-new">{currency}{props.new_price}</div>
+              <div className="item-price-old">{currency}{props.old_price}</div>
+            </div>
+          </div>
+        </Link>
+      </div>
+      {showToast && (
+        <div className="toast-notification">
+          <div className="toast-content">
+            <CheckCircleIcon className="toast-icon" />
+            <p className="toast-message">Product added to cart successfully!</p>
           </div>
         </div>
-      </Link>
-    </div>
+      )}
+    </>
   )
 }
 
